e1617d49da08f6213e8cb75806cdc6d5d922d7a7
Edson <EdsonP4100@gmail.com>
2025-07-29 20:08:19 -0700

    express validator

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,17 +1,37 @@
 import { Request, Response } from 'express';
+import slug from 'slug';
 import User from '../models/User';
+import { hashPassword } from '../utils/auth';
+import { validationResult } from 'express-validator';
 
 export const createAccount = async (req: Request, res: Response) => {
-  const { email } = req.body;
+  // Handle Errors
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
 
   const userExist = await User.findOne({ email });
 
   if (userExist) {
-    const error = new Error('El usuario ya está registrado');
+    const error = new Error('Email ya está registrado');
+    return res.status(409).json({ error: error.message });
+  }
+
+  const handle = slug(req.body.handle, '');
+
+  const handleExist = await User.findOne({ handle });
+
+  if (handleExist) {
+    const error = new Error('Nombre de usuario no disponible');
     return res.status(409).json({ error: error.message });
   }
 
   const user = new User(req.body);
+  user.password = await hashPassword(password);
+  user.handle = handle;
 
   await user.save();
   res.status(201).send('Registro creado correctamente');
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,25 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { createAccount } from './handlers';
 
 const router = Router();
 
 // Auth and Register Router
-router.post('/auth/register', createAccount);
+router.post(
+  '/auth/register',
+  body('handle').notEmpty().withMessage('El handle no puede ir vacío'),
+  body('name').notEmpty().withMessage('El nombre no puede ir vacío'),
+  body('email')
+    .isEmail()
+    .withMessage('E-mail no válido')
+    .notEmpty()
+    .withMessage('El E-mail no puede ir vacío'),
+  body('password')
+    .notEmpty()
+    .withMessage('El password no puede ir vacío')
+    .isLength({ min: 8 })
+    .withMessage('Mínimo 8 caracteres'),
+  createAccount
+);
 
 export default router;
