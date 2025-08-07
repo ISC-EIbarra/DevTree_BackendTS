012089c087f03d4b7685e31fa0bbde19b6413f68
Edson <EdsonP4100@gmail.com>
2025-08-06 18:34:30 -0700

    env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import authRouter from './router';
 import { connectDB } from './config/db';
+import { corsConfig } from './config/cors';
 
 const app = express();
 
 connectDB();
+
+// Cors
+app.use(cors(corsConfig));
 
 // Enabled form data reading
 app.use(express.json());
